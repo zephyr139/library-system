995f0ff5fa57f29c568f48207c18942a6e4ccb50
Oleksandr Rybalko <oleksandr.rybalko@student.tuke.sk>
2026-07-28 14:34:43 +0200

    initial commit

---
+++ src/test/java/com/training/librarysystem/LibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.librarysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
